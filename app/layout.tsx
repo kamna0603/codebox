545e22223e21f04cf6dd42a16a25bc9950f79f7d
Kamna chauhan <kamnac78@gmail.com>
2026-05-10 01:24:08 +0530

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono,Pixelify_Sans ,Inter} from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -10,6 +10,16 @@ const geistSans = Geist({
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+});
+
+const GameFont = Pixelify_Sans({
+  subsets:['latin'],
+  variable:'--font-game',
+});
+
+const inter=Inter({
+  subsets:["latin"],
+  variable:"--font-inter",
 });
 
 export const metadata: Metadata = {
@@ -25,7 +35,9 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${geistSans.variable} ${geistMono.variable}
+      ${GameFont.variable} ${inter.variable}
+       h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
